4d346ed67a09e531b7365a6749d81db2eae5bbce
Parag Chaudhary <parag.chaudhary@example.com>
2025-06-08 21:40:25 +0530

    Initial backend project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# book-app-backend
+//9vWOhjkXFCJoL76z mongodb pass
---
+++ index.js
@@ -0,0 +1,55 @@
+// index.js
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(cors({
+    origin: ['http://localhost:5173','https://parags-mern-project.vercel.app/'],
+    credentials: true
+}));
+
+const bookRoutes = require('./src/books/book.route');
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes = require("./src/users/user.route");
+const adminStatsRoutes = require("./src/stats/admin.stats");
+
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminStatsRoutes);
+
+const clientOptions = {
+    serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true
+    },
+};
+
+async function connectToMongoDB() {
+    try {
+        await mongoose.connect(process.env.DB_URL, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Mongodb connected successfully!");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    }
+}
+
+connectToMongoDB();
+
+app.use("/", (req, res) => {
+    res.send("Book Store Server is running!");
+});
+
+app.listen(port, () => {
+    console.log(`Book Store Server listening on port ${port}`);
+});
